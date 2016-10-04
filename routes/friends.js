5942017e4907b908b2d9770f0817372383011e2b
click <evgen.veprickiy@gmail.com>
2016-10-04 16:08:58 +0300

    add - routes (made them separately) chore - app

--- routes/friends.js
+++ routes/friends.js
@@ -1,33 +1,26 @@
 var express = require('express');
 var router = express.Router();
-var usersSchema = require('../models/usersSchema');
+var friendschat = require('../models/friendsSchema');
 
-// users
-router.get('/user', function (req, res) {
-    usersSchema.find(function (err, users) {
+// friends
+router.get('/friends', function (req, res) {
+    friendschat.find(function (err, users) {
         if (err) return console.error(err);
         console.log(users);
         res.send (users);
     });
 });
 
-router.post('/user', function (req, res) {
-    usersSchema(req.body).save(function (err) {
+router.post('/friends', function (req, res) {
+    friendschat(req.body).save(function (err) {
         if (err) return console.error(err);
         console.log('Added new User ' + req.body.login);
         res.send('Added new User ' + req.body.login);
     });
 });
 
-router.put('/user', function(req, res, next) {
-    usersSchema.findByIdAndUpdate(req.params.id, req.body, function (err, users) {
-        if (err) return next(err);
-        res.json(users);
-    });
-});
-
-router.delete('/user', function (req, res) {
-    usersSchema.find(req.body).remove(function (err) {
+router.delete('/friends', function (req, res) {
+    friendschat.find(req.body).remove(function (err) {
         if (err) return console.error(err);
         console.log('User ' + req.body.login + 'was removed');
         res.send('User ' + req.body.login + ' was removed');
